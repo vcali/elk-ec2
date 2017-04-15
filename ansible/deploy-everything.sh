a08600da998718800815f763a68288918397f861
Vitor Cali <vcali@localhost.localdomain>
2017-04-15 08:02:30 -0700

    organizing stuff

---
+++ ansible/deploy-everything.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This script will launch Ansible to deploy all the infra-structure on AWS and ELK Stack
+
+# Deploying AWS
+ansible-playbook /ansible/deploy_aws.yml
+
+# Installing ELK Stack
+ansible-playbook -i /ansible/ec2_hosts /ansible/install_elk.yml --private-key=/ansible/key-vcali-nvirg.pem --become-user=root --become-method=sudo
